588d6c278f65c849158ba0c0492019f0936b8797
mlntcandy <me@mlntcandy.com>
2023-10-15 18:36:48 +0300

    index gen (hopefully works)

---
+++ .github/workflows/generate-index.js
@@ -0,0 +1,47 @@
+const fs = require('node:fs/promises');
+
+const ignore = ['index.json', '.github', '.git'];
+
+const ezTomlParse = (toml, key) => {
+    try {
+        const path = key.split('.');
+        if (path.length > 1) {
+            toml = toml.split(path[0])[1];
+            return ezTomlParse(toml, path[1]);
+        }
+        const pairs = toml.split('\n').map((line) => line.split('=').map(t => t.trim()));
+        const val = pairs.filter((pair) => pair[0] === path[0])[0][1];
+        return eval(val);
+    } catch (e) {
+        return null;
+    }
+}
+
+const generateIndex = async () => {
+    const files = await fs.readdir('./');
+    const index = await Promise.all(files
+        .filter((dir) => !ignore.includes(dir))
+        .map(async (dir) => {
+            const toml = await fs.readFile(`${dir}/pack.toml`, 'utf8');
+            const obj = {
+                name: ezTomlParse(toml, 'name'),
+                author: ezTomlParse(toml, 'author'),
+                id: dir,
+                ...(await fs.stat(`${dir}/pack.png`).then(() => ({ icon: `${dir}/pack.png` })).catch(() => ({}))),
+                versions: {
+                    pack: ezTomlParse(toml, 'version'),
+                    pw: ezTomlParse(toml, 'pack-format'),
+                    mc: ezTomlParse(toml, 'versions.minecraft'),
+                    forge: ezTomlParse(toml, 'versions.forge'),
+                },
+                modCount: (await fs.readdir(`${dir}/mods`)).length,
+            }
+            console.log("Generated index for", obj.name, "@" + obj.versions.pack);
+            return obj;
+        })
+    )
+
+    await fs.writeFile('index.json', JSON.stringify(index, undefined, 2));
+}
+
+generateIndex();
