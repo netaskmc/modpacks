355774dd6e7690ee1f21fb06dfe3460f2b06e17a
mlntcandy <me@mlntcandy.com>
2023-10-15 19:28:55 +0300

    added jpg support, bugfixes

--- .github/workflows/generate-index.js
+++ .github/workflows/generate-index.js
@@ -6,7 +6,7 @@ const ezTomlParse = (toml, key) => {
     try {
         const path = key.split('.');
         if (path.length > 1) {
-            toml = toml.split(path[0])[1];
+            toml = toml.split(`[${path[0]}]`)[1];
             return ezTomlParse(toml, path[1]);
         }
         const pairs = toml.split('\n').map((line) => line.split('=').map(t => t.trim()));
@@ -23,11 +23,16 @@ const generateIndex = async () => {
         .filter((dir) => !ignore.includes(dir))
         .map(async (dir) => {
             const toml = await fs.readFile(`${dir}/pack.toml`, 'utf8');
+
+            let icon = (await fs.stat(`${dir}/pack.png`).then(() => `${dir}/pack.png`).catch(() => null));
+            if (!icon) icon = (await fs.stat(`${dir}/pack.jpg`).then(() => `${dir}/pack.jpg`).catch(() => null));
+
+            if (!ezTomlParse(toml, 'name')) return null;
             const obj = {
                 name: ezTomlParse(toml, 'name'),
                 author: ezTomlParse(toml, 'author'),
                 id: dir,
-                ...(await fs.stat(`${dir}/pack.png`).then(() => ({ icon: `${dir}/pack.png` })).catch(() => ({}))),
+                icon,
                 versions: {
                     pack: ezTomlParse(toml, 'version'),
                     pw: ezTomlParse(toml, 'pack-format'),
@@ -36,12 +41,13 @@ const generateIndex = async () => {
                 },
                 modCount: (await fs.readdir(`${dir}/mods`)).length,
             }
+
             console.log("Generated index for", obj.name, "@" + obj.versions.pack);
             return obj;
         })
     )
 
-    await fs.writeFile('index.json', JSON.stringify(index, undefined, 2));
+    await fs.writeFile('index.json', JSON.stringify(index.filter(pack => pack !== null), undefined, 2));
 }
 
 generateIndex();
